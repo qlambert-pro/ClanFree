1b5d8caa9beebd4bc126b0a62519edba2468d142
Quentin Lambert <lambert.quentin@gmail.com>
2015-04-18 11:57:25 +0200

    init with an empty gdx repo

---
+++ desktop/src/com/clanfree/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.clanfree.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.clanfree.game.ClanFree;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ClanFree(), config);
+	}
+}
